c0ae9db81517d2850faa4d3c69dfbb4244add347
Matthew Corner <mcorner@signiant.com>
2015-11-25 10:26:19 -0500

    cache: Finished initialization method of cache

--- signiant/cache.py
+++ signiant/cache.py
@@ -35,31 +35,46 @@ class LocalCache(object):
     name = None
     local_path = None
     settings_file = None
+    remote_url = None
 
     #Verifies local existance
     def __init__(self, cache_root):
         if not os.path.exists(cache_root):
-            raise CacheError("The path to the specified cache does not exist.")
+            raise CacheError("The path '" + str(cache_root) + "' does not contain a valid umpire cache.")
         
+        self.local_path = cache_root
+
         self.settings_file = os.path.join(cache_root,CONFIG_FILENAME)
 
         if not os.path.exists(self.settings_file):
             raise CacheError("The specified path does not appear to be a valid Umpire cache")
         config = None
         
-        try:
-            parser = ConfigParser.SafeConfigParser()
+        #Get parser
+        parser = ConfigParser.SafeConfigParser()
+        try:#Read
             config = parser.read(self.settings_file)
         except ConfigParser.ParsingError:
             raise CacheError("Error parsing the cache configuration file. Please contact an administrator for help fixing this problem.")
 
-        if config is None or len(config.sections()) == 0:
-            raise CacheError("The config file appears to be empty. Please contact an administrator for help fixing this problem.")
+        #Verify the config has been read 
+        if config is None:
+            raise CacheError("The config file appears to be empty. Please verify this is the location of a valid umpire cache.")
 
+        #Verify the section we're looking for is there 
+        if CONFIG_SECTION_NAME not in config.sections():
+            raise CacheError("The config file appears to be missing the [umpire] section. Please verify this is the location of a valid umpire cache.")
+        
+        #Attempt to parse the data
         try:
-            parser = 
+            self.remote_url = config.get(CONFIG_SECTION_NAME, "remote_url")
+        except ConfigParser.Error:
+            raise CacheError("Unable to determine the name of this cache")
 
-    
+        if not self.remote_url:
+            raise CacheError("Unable to determine the name of this cache")
+
+
     #Gets local entry, returns None if it doesn't exist.
     def get(platform, name, version):
         pass
