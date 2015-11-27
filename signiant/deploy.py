387c096ceda0f8bba3c2e4771a049fdeab7b335c
Matthew Corner <mcorner@signiant.com>
2015-11-27 13:58:35 -0500

    deploy,unpack: Added unpacking logic for demo

--- signiant/deploy.py
+++ signiant/deploy.py
@@ -14,6 +14,8 @@ from multiprocessing import Value
 from maestro.internal import module
 from maestro.aws import s3
 
+import unpack
+
 HELP_KEYS = ["h", "help"]
 
 class DeploymentModule(module.AsyncModule):
@@ -42,20 +44,44 @@ class DeploymentModule(module.AsyncModule):
                 destination = item["destination"]
 
                 full_url = s3.join_s3_url(repo_url, platform, name, version)
-                print full_url
                 downloader = s3.AsyncS3Downloader(None)
                 downloader.source_url = full_url
                 downloader.destination_path = destination
+                print "Downloading " + full_url
                 downloader.start(None)
                 downloaders.append(downloader)
+
+        unpacker_count = 0
+        unpackers = list()
 
         downloader_count = 0
         while downloader_count < len(downloaders):
             downloader_count = 0
             for downloader in downloaders:
                 if downloader.status == module.DONE:
+                    for item in downloader.result:
+                        if item.endswith(".tar.gz"):
+                            unpacker = unpack.UnpackModule(None)
+                            unpacker.file_path = item
+                            unpacker.destination_path = os.path.split(item)[0]
+                            unpacker.delete_archive = True
+                            print "Unpacking " + item
+                            unpacker.start(None)
+                            unpackers.append(unpacker)
+                            downloader.status = 123
+                if downloader.status == 123:
                     downloader_count += 1
-            time.sleep(1)
+            time.sleep(0.1)
+
+        while unpacker_count < len(downloaders):
+            unpacker_count = 0
+            for unpacker in unpackers:
+                if unpacker.exception is not None:
+                    print "Error in unpacker: " + str(unpacker.exception)
+                if unpacker.status == module.DONE:
+                    unpacker_count += 1
+            time.sleep(0.1)
+            
         print "Done!"
 
 if __name__ == "__main__":
