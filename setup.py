9bcce4d6e32177b8b600e88be471ab018a49e609
Matthew Corner <mcorner@signiant.com>
2015-12-10 15:31:27 -0500

    general: Added python3 style print statements fetch: Fixed an error where we were specifically looking for tar.gz files, removed the if statement

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='Umpire',
-      version='0.1.1',
+      version='0.2.0a2',
       description='Generic dependency resolver.',
       author='Matthew Corner',
       author_email='mcorner@signiant.com',
       url='https://www.signiant.com',
       packages=find_packages(),
       license='MIT',
-      install_requires=['MaestroOps>=0.2.2'],
+      install_requires=['MaestroOps>=0.3.0'],
       entry_points = {
           'console_scripts': [
               'umpire = umpire.umpire:entry'
