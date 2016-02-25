253397f68021b89cffab453c0a95f37c09c81be1
Matthew Corner <mcorner@signiant.com>
2016-02-24 23:56:24 -0500

    update: Provided simple update system

--- umpire/umpire.py
+++ umpire/umpire.py
@@ -3,10 +3,12 @@ from maestro.core import execute
 from maestro.aws import s3
 from maestro.tools import path
 
+__version__ = "0.3.0"
+
 import sys,os
 
 #Internal Modules
-from . import fetch, unpack, deploy
+from . import fetch, unpack, deploy, update
 
 #Config
 from . import config
@@ -18,6 +20,8 @@ def entry():
 
 class Umpire(execute.ModuleExecuter):
 
+    skip_update = False
+
     #
     # !!!Register all the dependencies of this program here.!!!
     #
@@ -26,45 +30,73 @@ class Umpire(execute.ModuleExecuter):
         self.register(fetch.FetchModule())
         self.register(unpack.UnpackModule())
         self.register(deploy.DeploymentModule())
+        self.register(update.UpdateModule())
 
     def run(self, kwargs):
-        #TODO: Add these as modules
+
+        #TODO: Add these as modules (or a module)
         for index, item in enumerate(sys.argv):
             if index == 0:
                 continue
             if item == "-c" or item == "--clear-cache":
                 import shutil
-                shutil.rmtree(self.get_umpire_root())
+                shutil.rmtree(get_umpire_root())
                 sys.exit(0)
             elif item == "-r" or item == "--repair-cache":
-                path.purge(".umplock", self.get_umpire_root())
+                path.purge(".umplock", get_umpire_root())
                 sys.exit(0)
+            elif item == "-s" or item == "--skip-update":
+                self.skip_update = True
+
 
         self.register_dependencies()
+        if not self.skip_update:
+            updater = self.getObject("update")
+            try:
+                #Check if there's an update available
+                with open(os.path.join(config.REMOTE_VERSION_FILENAME), "r") as f:
+                    version = f.read()
+                    rmajor, rminor, rrev = update.parse_version_string(version)
+                    major, minor, rev = update.parse_version_string(__version__)
+                    print (str(rmajor) + str(rminor) + str(rrev))
+                    print (str(major) + str(minor) + str(rev))
+                    if (rmajor > major) or ((rminor > minor) and (rmajor == rmajor)) or ((rrev > rev) and (rminor == minor) and (rmajor == rmajor)):
+                        if config.autoupdate:
+                            print("\n!!!Update (%s) detected!!!\n" % version[:-1])
+                            print("Autoupdating...\n")
+                            updater.run({"update":None})
+                            sys.exit(0)
+                        else:
+                            print("!!!Update (%s) detected!!!" % version[:-1])
+                            print("Please run pip install umpire --upgrade as an Administrator.\n")
+                            updater.start()
+            except IOError:
+                updater.start()
+
+
 
         #Get the instance of our first module
         deployer = self.getObject("deploy")
 
         #TODO: Fix to use just RUN
-        deployer.cache_root = self.get_umpire_root()
+        deployer.cache_root = get_umpire_root()
 
         try:
             #Run it
             self.exit_code = deployer.run(kwargs)
         except KeyboardInterrupt:
             print("Got KeyboardInterrupt. Cleaning lock files.")
-            path.purge(".umplock", self.get_umpire_root())
+            path.purge(".umplock", get_umpire_root())
             sys.exit(1)
 
-
-    def get_umpire_root(self):
-        hardcoded_root = "./umpire_tmp"
-        try:
-            if not os.path.exists(config.default_umpire_root):
-                os.makedirs(config.default_umpire_root)
-            return config.default_umpire_root
-        except Exception as e:
-            print("Umpire: Error obtaining default umpire root location, using local directory './umpire_tmp': " + str(e), file=sys.stderr)
-            if not os.path.exists(hardcoded_root):
-                os.makedirs(hardcoded_root)
-            return hardcoded_root
+def get_umpire_root():
+    hardcoded_root = "./umpire_tmp"
+    try:
+        if not os.path.exists(config.default_umpire_root):
+            os.makedirs(config.default_umpire_root)
+        return config.default_umpire_root
+    except Exception as e:
+        print("Umpire: Error obtaining default umpire root location, using local directory './umpire_tmp': " + str(e), file=sys.stderr)
+        if not os.path.exists(hardcoded_root):
+            os.makedirs(hardcoded_root)
+        return hardcoded_root
