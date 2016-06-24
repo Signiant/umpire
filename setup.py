c9508fb83100a9bf39f3797e3b18eba1a6db11cb
Matthew Corner <mcorner@signiant.com>
2016-06-24 12:58:37 -0400

    Bumped version to 0.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='Umpire',
-      version='0.4.3',
+      version='0.4.4',
       description='Generic dependency resolver.',
       author='Matthew Corner',
       author_email='mcorner@signiant.com',
--- umpire/umpire.py
+++ umpire/umpire.py
@@ -3,7 +3,7 @@ from maestro.core import execute
 from maestro.aws import s3
 from maestro.tools import path
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 import sys,os
 
