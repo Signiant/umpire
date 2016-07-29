4d937519029698b877d4260e9c68cdbb2aaa1070
Matthew Corner <mcorner@signiant.com>
2016-07-28 20:30:14 -0400

    Bumped version numbers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='Umpire',
-      version='0.5.0a4',
+      version='0.5.0',
       description='Generic dependency resolver.',
       author='Matthew Corner',
       author_email='mcorner@signiant.com',
