b14a62c9c3b9e4477ea22b5191962e10d784836a
Kevin Haggerty <khaggerty@signiant.com>
2019-06-20 08:30:30 -0400

    Updating version after publishing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='Umpire',
-      version='0.5.5',
+      version='0.5.6',
       description='Generic dependency resolver.',
       author='Matthew Corner',
       author_email='mcorner@signiant.com',
