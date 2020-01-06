f50f587f0cf4055eb769e00610274b8e2f1add21
William Jia <wjia@signiant.com>
2020-01-06 15:08:16 -0500

    update version number

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 from distutils.core import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='Umpire',
-      version='0.5.6',
+      version='0.6.0',
       description='Generic dependency resolver.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Matthew Corner',
       author_email='mcorner@signiant.com',
       url='https://www.signiant.com',
